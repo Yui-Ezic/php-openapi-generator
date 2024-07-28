0da4bc2e7c3bb4d992bcf1616b800ffc4801cf33
Mykhailo Zuiev <mishan221199@gmail.com>
2024-07-28 20:11:19 +0200

    fix: urlEncode transformer does not call next()

--- testProject/src/QuerySerializer/Transformer/UrlEncode.php
+++ testProject/src/QuerySerializer/Transformer/UrlEncode.php
@@ -6,7 +6,7 @@ class UrlEncode
 {
     public function __invoke(string $value, callable $next): string
     {
-        return rawurlencode($value);
+        return rawurlencode($next());
     }
 
 }
